8457c2309a2d75346fd1832038c6763f38c7a808
Marcial Fernandez <marcial@larces.uece.br>
2020-01-10 17:05:29 -0300

    Update spark-env.sh

--- spark-conf/spark-env.sh
+++ spark-conf/spark-env.sh
@@ -2,3 +2,7 @@
 export MESOS_NATIVE_JAVA_LIBRARY=${MESOS_NATIVE_JAVA_LIBRARY:-/usr/lib/libmesos.so}
 export SPARK_LOCAL_IP=${SPARK_LOCAL_IP:-"127.0.0.1"}
 export SPARK_PUBLIC_DNS=${SPARK_PUBLIC_DNS:-"127.0.0.1"}
+export PYSPARK_PYTHON=python3
+export PATH=$PATH:/usr/lib/spark/bin/
+export SPARK_HOME=/usr/lib/spark
+
