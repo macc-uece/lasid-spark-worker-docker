ae61b073efb077e8bfee9982a95e8a0cbf6a668e
Marcial Fernandez <marcial@larces.uece.br>
2020-01-19 11:26:15 -0300

    Update spark-env.sh

--- docker/spark-worker/spark-env.sh
+++ docker/spark-worker/spark-env.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
 
+export JAVA_HOME=/usr/lib/jvm/default-java
+export PYSPARK_PYTHON=/usr/bin/python
+export PYSPARK_DRIVER_PYTHON=/usr/bin/python
+export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
+export LIBPROCESS_IP="10.129.64.20"
+export SPARK_LOCAL_IP="10.129.64.20"
+export SPARK_PUBLIC_DNS="10.129.64.20"
+export SPARK_MASTER_WEBUI_PORT=8000
