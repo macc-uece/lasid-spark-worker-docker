24709091ff3a1ba93002a4487b68cf43673f9a33
Marcial Fernandez <marcial@larces.uece.br>
2020-01-19 15:19:07 -0300

    Update start-worker.sh

--- docker/spark-worker/start-worker.sh
+++ docker/spark-worker/start-worker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+export SPARK_HOME=/usr/lib/spark
+
 . "/spark/sbin/spark-config.sh"
 . "/spark/bin/load-spark-env.sh"
 
 mkdir -p $SPARK_WORKER_LOG
-
-export SPARK_HOME=/usr/lib/spark
 
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 
