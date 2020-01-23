5c843187c432e297bc03b221ab4b23555811f3cb
Marcial Fernandez <marcial@larces.uece.br>
2020-01-23 18:43:47 -0300

    Update run.sh

--- docker/spark-mesos/run.sh
+++ docker/spark-mesos/run.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-SPARK_MASTER=${SPARK_MASTER:-localhost}
-MESOS_EXECUTOR_CORE=${MESOS_EXECUTOR_CORE:-0.1}
-SPARK_IMAGE=${SPARK_IMAGE:-lasid/spark:lastest}
+#SPARK_MASTER=${SPARK_MASTER:-localhost}
+#MESOS_EXECUTOR_CORE=${MESOS_EXECUTOR_CORE:-0.1}
+#SPARK_IMAGE=${SPARK_IMAGE:-lasid/spark:lastest}
 CURRENT_IP=$(hostname -i)
 
-sed -i 's;SPARK_MASTER;'$SPARK_MASTER';g' /usr/lib/spark/conf/spark-defaults.conf
-sed -i 's;MESOS_EXECUTOR_CORE;'$MESOS_EXECUTOR_CORE';g' /usr/lib/spark/conf/spark-defaults.conf
-sed -i 's;SPARK_IMAGE;'$SPARK_IMAGE';g' /usr/lib/spark/conf/spark-defaults.conf
-sed -i 's;CURRENT_IP;'$CURRENT_IP';g' /usr/lib/spark/conf/spark-defaults.conf
+#sed -i 's;SPARK_MASTER;'$SPARK_MASTER';g' /usr/lib/spark/conf/spark-defaults.conf
+#sed -i 's;MESOS_EXECUTOR_CORE;'$MESOS_EXECUTOR_CORE';g' /usr/lib/spark/conf/spark-defaults.conf
+#sed -i 's;SPARK_IMAGE;'$SPARK_IMAGE';g' /usr/lib/spark/conf/spark-defaults.conf
+#sed -i 's;CURRENT_IP;'$CURRENT_IP';g' /usr/lib/spark/conf/spark-defaults.conf
 
-export SPARK_LOCAL_IP=${SPARK_LOCAL_IP:-${CURRENT_IP:-"127.0.0.1"}}
-export SPARK_PUBLIC_DNS=${SPARK_PUBLIC_DNS:-${SPARK_PUBLIC_DNS:-"127.0.0.1"}}
+#export SPARK_LOCAL_IP=${SPARK_LOCAL_IP:-${CURRENT_IP:-"127.0.0.1"}}
+#export SPARK_PUBLIC_DNS=${SPARK_PUBLIC_DNS:-${SPARK_PUBLIC_DNS:-"127.0.0.1"}}
+export SPARK_MASTER_IP=10.129.64.20
+
 export PYSPARK_PYTHON=python3
 export PATH=$PATH:/usr/lib/spark/bin/
 export SPARK_HOME=/usr/lib/spark
