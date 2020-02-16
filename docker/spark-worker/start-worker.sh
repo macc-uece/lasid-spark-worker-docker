139201ee7e926e444d70e519cc7ec6523daf66c4
Marcial Fernandez <marcial@larces.uece.br>
2020-02-16 18:48:35 -0300

    Update start-worker.sh

--- docker/spark-worker/start-worker.sh
+++ docker/spark-worker/start-worker.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 
-SPARKMASTER=${SPARKMASTER:-"mesos://zk://10.129.64.20:2181,10.129.64.10:2181,10.129.64.30:2181/mesos"}
-MESOSEXECUTORCORE=${MESOSEXECUTORCORE:-0.1}
-SPARKIMAGE=${SPARKIMAGE:-"lasid/spark-worker:latest"}
-CURRENTIP=$(hostname -i)
-SPARK_WORKER_LOG=${SPARK_WORKER_LOG:-"/var/log/spark"}
-
-sed -i 's;SPARKMASTER;'$SPARKMASTER';g' /usr/lib/spark/conf/spark-defaults.conf
-sed -i 's;MESOSEXECUTORCORE;'$MESOSEXECUTORCORE';g' /usr/lib/spark/conf/spark-defaults.conf
-sed -i 's;SPARKIMAGE;'$SPARKIMAGE';g' /usr/lib/spark/conf/spark-defaults.conf
-sed -i 's;CURRENTIP;'$CURRENTIP';g' /usr/lib/spark/conf/spark-defaults.conf
-
+export SPARKMASTER=${SPARKMASTER:-"mesos://zk://10.129.64.20:2181,10.129.64.10:2181,10.129.64.30:2181/mesos"}
+export MESOSEXECUTORCORE=${MESOSEXECUTORCORE:-0.1}
+export SPARKIMAGE=${SPARKIMAGE:-"lasid/spark-worker:latest"}
+export CURRENTIP=$(hostname -i)
+export SPARK_WORKER_LOG=${SPARK_WORKER_LOG:-"/var/log/spark"}
 export SPARKLOCALIP=${SPARKLOCALIP:-${CURRENTIP:-"127.0.0.1"}}
 export SPARKPUBLICDNS=${SPARKPUBLICDNS:-${CURRENTIP:-"127.0.0.1"}}
 export SPARK_HOME=${SPARK_HOME:-"/usr/lib/spark"}
