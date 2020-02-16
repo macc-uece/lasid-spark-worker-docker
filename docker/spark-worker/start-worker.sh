a9e769a896b9b431bde0aa9331cf5c346dc14365
Marcial Fernandez <marcial@larces.uece.br>
2020-02-16 17:12:52 -0300

    Update start-worker.sh

--- docker/spark-worker/start-worker.sh
+++ docker/spark-worker/start-worker.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-export SPARK_HOME=/usr/lib/spark
+SPARKMASTER=${SPARKMASTER:-"mesos://zk://10.129.64.20:2181,10.129.64.10:2181,10.129.64.30:2181/mesos"}
+MESOSEXECUTORCORE=${MESOSEXECUTORCORE:-0.1}
+SPARKIMAGE=${SPARKIMAGE:-"lasid/spark-worker:latest"}
+CURRENTIP=$(hostname -i)
+SPARK_WORKER_LOG=${SPARK_WORKER_LOG:-"/usr/log/spark"}
+
+sed -i 's;SPARKMASTER;'$SPARKMASTER';g' /usr/lib/spark/conf/spark-defaults.conf
+sed -i 's;MESOSEXECUTORCORE;'$MESOSEXECUTORCORE';g' /usr/lib/spark/conf/spark-defaults.conf
+sed -i 's;SPARKIMAGE;'$SPARKIMAGE';g' /usr/lib/spark/conf/spark-defaults.conf
+sed -i 's;CURRENTIP;'$CURRENTIP';g' /usr/lib/spark/conf/spark-defaults.conf
+
+export SPARKLOCALIP=${SPARKLOCALIP:-${CURRENTIP:-"127.0.0.1"}}
+export SPARKPUBLICDNS=${SPARKPUBLICDNS:-${CURRENTIP:-"127.0.0.1"}}
+export SPARK_HOME=${SPARK_HOME:-"/usr/lib/spark"}
 export PATH=$PATH:$SPARK_HOME/bin
 
 cd $SPARK_HOME
@@ -12,7 +25,7 @@ mkdir -p $SPARK_WORKER_LOG
 
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 
-/usr/lib/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+#/usr/lib/spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
 ###!/bin/bash
 
 exec "$@"
