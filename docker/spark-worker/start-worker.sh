cab42883072e1bd86bed2bd2f63596ab78a5dfe5
Marcial Fernandez <marcial@larces.uece.br>
2020-04-04 18:52:04 -0300

    Update start-worker.sh

--- docker/spark-worker/start-worker.sh
+++ docker/spark-worker/start-worker.sh
@@ -25,6 +25,6 @@ mkdir -p $SPARK_WORKER_LOG
 
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker-$CURRENTIP.out
 
-#/usr/lib/spark/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+#exec "$@"
 
-exec "$@"
+/usr/lib/spark/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
