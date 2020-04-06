f3f32a86b6dd85631fe846cbe9a12db4194070c6
Marcial Fernandez <marcial@larces.uece.br>
2020-04-06 09:36:11 -0300

    Update start-worker.sh

--- docker/spark-worker/start-worker.sh
+++ docker/spark-worker/start-worker.sh
@@ -10,12 +10,16 @@ export MESOSEXECUTORCORE=${MESOSEXECUTORCORE:-0.1}
 #export SPARKIMAGE=${SPARKIMAGE:-"lasid/spark-worker:latest"}
 export CURRENTIP=$(hostname -i)
 export SPARK_LOCAL_IP=$CURRENTIP
+export SPARK_PUBLIC_DNS=$CURRENTIP
 export SPARK_WORKER_LOG=${SPARK_WORKER_LOG:-"/var/log/spark"}
 export SPARK_WORKER_WEBUI_PORT=${SPARK_WORKER_WEBUI_PORT:-"8081"}
 #export SPARKLOCALIP=${SPARKLOCALIP:-${CURRENTIP:-"127.0.0.1"}}
 #export SPARKPUBLICDNS=${SPARKPUBLICDNS:-${CURRENTIP:-"127.0.0.1"}}
 export SPARK_HOME=${SPARK_HOME:-"/usr/lib/spark"}
 export PATH=$PATH:$SPARK_HOME/bin
+
+#at container startup:
+echo -e "0.0.0.0 ${CURRENTIP}" >> /etc/hosts
 
 cd $SPARK_HOME
 
@@ -25,6 +29,6 @@ mkdir -p $SPARK_WORKER_LOG
 
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker-$CURRENTIP.out
 
-#exec "$@"
+#/usr/lib/spark/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
 
-/usr/lib/spark/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+exec "$@"
