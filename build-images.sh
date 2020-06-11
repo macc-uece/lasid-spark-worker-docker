d09d5e3aa9b7b97771ee0cf74c159aaaed23552c
Marcial Fernandez <marcial@larces.uece.br>
2020-06-11 16:03:34 -0300

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -4,7 +4,7 @@ set -e
 
 docker build -t lasid/spark-base:latest ./docker/spark-base
 docker build -t lasid/spark-worker:latest ./docker/spark-worker
-docker build -t lasid/spark-worker-mesos:latest ./docker/spark-worker-mesos
+#docker build -t lasid/spark-worker-mesos:latest ./docker/spark-worker-mesos
 docker login
 docker push lasid/spark-worker:latest
-docker push lasid/spark-worker-mesos:latest
+#docker push lasid/spark-worker-mesos:latest
