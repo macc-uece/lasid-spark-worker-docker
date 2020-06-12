bcf10803dd102a767eb899264cc13ffffbe3d00f
Marcial Fernandez <marcial@larces.uece.br>
2020-06-12 14:13:34 -0300

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+SPARK_VER="2.4.5"
+UBUNTU_VER="_bionic"
+
 set -e
 
-docker build -t lasid/spark-base:latest ./docker/spark-base
-docker build -t lasid/spark-worker:latest ./docker/spark-worker
+## Build Docker image
+docker build -t lasid/spark-base -t lasid/spark-base:$SPARK_VER$UBUNTU_VER ./docker/spark-base
+docker build -t lasid/spark-worker -t lasid/spark-worker:$SPARK_VER$UBUNTU_VER ./docker/spark-worker
 #docker build -t lasid/spark-worker-mesos:latest ./docker/spark-worker-mesos
+
+## Push image to DockerHub
 docker login
 docker push lasid/spark-worker:latest
+docker push lasid/spark-worker:$SPARK_VER$UBUNTU_VER
 #docker push lasid/spark-worker-mesos:latest
