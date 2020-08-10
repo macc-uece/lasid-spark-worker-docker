5c153b3f032a14d4b4488988c3e72c6a3217e998
Marcial Fernandez <marcial@larces.uece.br>
2020-08-10 20:41:23 -0300

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SPARK_VER="2.4.5"
+SPARK_VER="2.4.6"
 UBUNTU_VER="_bionic"
 
 set -e
@@ -8,10 +8,13 @@ set -e
 ## Build Docker image
 docker build -t lasid/spark-base -t lasid/spark-base:$SPARK_VER$UBUNTU_VER ./docker/spark-base
 docker build -t lasid/spark-worker -t lasid/spark-worker:$SPARK_VER$UBUNTU_VER ./docker/spark-worker
+docker build -t lasid/spark-worker-tensorflow -t lasid/spark-worker-tensorflow:$SPARK_VER$UBUNTU_VER ./docker/spark-worker-tensorflow
 #docker build -t lasid/spark-worker-mesos:latest ./docker/spark-worker-mesos
 
 ## Push image to DockerHub
 docker login
 docker push lasid/spark-worker:latest
 docker push lasid/spark-worker:$SPARK_VER$UBUNTU_VER
+docker push lasid/spark-worker-tensorflow:latest
+docker push lasid/spark-worker-tensorflow:$SPARK_VER$UBUNTU_VER
 #docker push lasid/spark-worker-mesos:latest
