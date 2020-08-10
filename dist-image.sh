ea08588596270f52db3b621dbe40fddcfa31b4bc
marcialf <marcial@larces.uece.br>
2020-08-10 20:47:06 -0300

    Script to distribute image in cluster

---
+++ dist-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Download docker image in all servers
+
+ssh -tt stack@lasid10 "docker pull lasid/spark-worker"
+ssh -tt stack@lasid30 "docker pull lasid/spark-worker"
+ssh -tt stack@lasid40 "docker pull lasid/spark-worker"
+ssh -tt stack@lasid50 "docker pull lasid/spark-worker"
+ssh -tt stack@lasid60 "docker pull lasid/spark-worker"
+ssh -tt stack@lasid70 "docker pull lasid/spark-worker"
+ssh -tt stack@lasid10 "docker pull lasid/spark-worker-tensorflow"
+ssh -tt stack@lasid30 "docker pull lasid/spark-worker-tensorflow"
+ssh -tt stack@lasid40 "docker pull lasid/spark-worker-tensorflow"
+ssh -tt stack@lasid50 "docker pull lasid/spark-worker-tensorflow"
+ssh -tt stack@lasid60 "docker pull lasid/spark-worker-tensorflow"
+ssh -tt stack@lasid70 "docker pull lasid/spark-worker-tensorflow"
