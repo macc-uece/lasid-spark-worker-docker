010057adfcb0788c53d74b7c1145f9835fed1711
marcialf <marcial@larces.uece.br>
2020-09-29 15:01:30 -0300

    Set Spark 3 configuration

---
+++ docker/spark-worker/spark-env_3.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export SPARK_HOME=/usr/lib/spark
+export PYSPARK_PYTHON=/usr/bin/python
+export PYSPARK_DRIVER_PYTHON=/usr/bin/python
+export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python
+#export LIBPROCESS_IP=10.129.64.20
+#export SPARK_MASTER_IP=10.129.64.20
+#export SPARK_MASTER_HOST=10.129.64.20
+export PATH=$PATH:$SPARK_HOME/bin
