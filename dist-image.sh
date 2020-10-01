035275142be9b415c70f1773f9e3de3c52441720
Marcial Fernandez <marcial@larces.uece.br>
2020-10-01 17:51:29 -0300

    Update dist-image.sh

--- dist-image.sh
+++ dist-image.sh
@@ -2,15 +2,28 @@
 #
 # Download docker image in all servers
 
-ssh -tt stack@lasid10 "docker pull lasid/spark-worker"
-ssh -tt stack@lasid30 "docker pull lasid/spark-worker"
-ssh -tt stack@lasid40 "docker pull lasid/spark-worker"
-ssh -tt stack@lasid50 "docker pull lasid/spark-worker"
-ssh -tt stack@lasid60 "docker pull lasid/spark-worker"
-ssh -tt stack@lasid70 "docker pull lasid/spark-worker"
-ssh -tt stack@lasid10 "docker pull lasid/spark-worker-tensorflow"
-ssh -tt stack@lasid30 "docker pull lasid/spark-worker-tensorflow"
-ssh -tt stack@lasid40 "docker pull lasid/spark-worker-tensorflow"
-ssh -tt stack@lasid50 "docker pull lasid/spark-worker-tensorflow"
-ssh -tt stack@lasid60 "docker pull lasid/spark-worker-tensorflow"
-ssh -tt stack@lasid70 "docker pull lasid/spark-worker-tensorflow"
+if [ $1 == '3' ] ; then
+	SPARK_VER="3.0.1"
+elif [ $1 == '2' ] ; then
+	SPARK_VER="2.4.7"
+else
+	echo "Script to build LASID Spark Docker image."
+	echo "Usage: ./build-images.sh <Spark> " 
+	echo "  <Spark> Spark major number version, i.e., 2 or 3" 
+	exit 1
+fi
+
+UBUNTU_VER="bionic"
+
+ssh -tt stack@lasid10 "docker pull lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid30 "docker pull lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid40 "docker pull lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid50 "docker pull lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid60 "docker pull lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid70 "docker pull lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid10 "docker pull lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid30 "docker pull lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid40 "docker pull lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid50 "docker pull lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid60 "docker pull lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}"
+ssh -tt stack@lasid70 "docker pull lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}"
