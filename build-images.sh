de98bed99b7c688932ac9bd521f43e2bf6791075
Marcial Fernandez <marcial@larces.uece.br>
2020-10-01 17:43:25 -0300

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -8,7 +8,7 @@ if [ $1 == '3' ] ; then
 elif [ $1 == '2' ] ; then
 	SPARK_VER="2.4.7"
 	JAVA_VER=8 
-        SPARK_GRAPH_PKG="0.8.1-spark3.0-s_2.12/graphframes-0.8.1-spark3.0-s_2.12.jar" 
+        SPARK_GRAPH_PKG="0.8.1-spark2.4-s_2.12/graphframes-0.8.1-spark2.4-s_2.12.jar" 
         SPARK_DELTA_PKG="delta-core_2.12/0.7.0/delta-core_2.12-0.7.0.jar"
 else
 	echo "Script to build LASID Spark Docker image."
@@ -26,12 +26,12 @@ set -e
 docker build --build-arg SP_VERSION=${SPARK_VER} --build-arg JV_VERSION=${JAVA_VER} --build-arg SC_VERSION=${SCALA_VERSION} \
              --build-arg SP_G_PKG=${SPARK_GRAPH_PKG} --build-arg SP_D_PKG=${SPARK_DELTA_PKG} \
              -t lasid/spark-base:${SPARK_VER}_${UBUNTU_VER} ./docker/spark-base
-#docker build --build-arg SP_VERSION=${SPARK_VER} -t lasid/spark-worker -t lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER} ./docker/spark-worker
-#docker build --build-arg SP_VERSION=${SPARK_VER} -t lasid/spark-worker-tensorflow -t lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER} ./docker/spark-worker-tensorflow
+docker build --build-arg SP_VERSION=${SPARK_VER} -t lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER} ./docker/spark-worker
+docker build -t lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER} ./docker/spark-worker-tensorflow
 
 ## Push image to DockerHub
-#docker login
+docker login
 #docker push lasid/spark-worker:latest
-#docker push lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}
+docker push lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}
 #docker push lasid/spark-worker-tensorflow:latest
-#docker push lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}
+docker push lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}
