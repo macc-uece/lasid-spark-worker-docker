1b306347676d9808d6eaed7c8f0d83e84764a2c4
Marcial Fernandez <marcial@larces.uece.br>
2020-10-13 12:10:28 -0300

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -27,9 +27,11 @@ set -e
 ## Build Docker image
 docker build --build-arg SP_VERSION=${SPARK_VER} --build-arg JV_VERSION=${JAVA_VER} --build-arg SC_VERSION=${SCALA_VERSION} \
              --build-arg HD_VERSION=${HADOOP_VER} --build-arg SP_G_PKG=${SPARK_GRAPH_PKG} --build-arg SP_D_PKG=${SPARK_DELTA_PKG} \
-             -t lasid/spark-base:${SPARK_VER}_${UBUNTU_VER} ./docker/spark-base
-docker build --build-arg SP_VERSION=${SPARK_VER} -t lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER} ./docker/spark-worker
-docker build -t lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER} ./docker/spark-worker-tensorflow
+             -t lasid/spark-base:${SPARK_VER}_${UBUNTU_VER}  -t lasid/spark-base:latest ./docker/spark-base
+docker build --build-arg SP_VERSION=${SPARK_VER} -t lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER} \
+             -t lasid/spark-worker:latest ./docker/spark-worker
+docker build -t lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER} -t lasid/spark-worker-tensorflow:latest \
+             ./docker/spark-worker-tensorflow
 
 ## Push image to DockerHub
 docker login
