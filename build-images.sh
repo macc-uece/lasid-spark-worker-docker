b0b5e5cf5cf25c2ef5de4df637f1b2ded7c74c61
marcialf <marcial@larces.uece.br>
2022-02-19 18:35:37 -0300

    include Kubernetes compatibility

--- build-images.sh
+++ build-images.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 if [ $1 == '3' ] ; then
-	SPARK_VER="3.2.0"
-	JAVA_VER="11"
-	HADOOP_VER="3.2"
-	SCALA_VERSION="2.12.15"
-       	SPARK_GRAPH_PKG="0.8.2-spark3.2-s_2.12/graphframes-0.8.2-spark3.2-s_2.12.jar"
-	SPARK_DELTA_PKG="delta-core_2.12/1.0.0/delta-core_2.12:1.0.0r"
-	SPARK_GRAPH_JAR="graphframes:graphframes:0.8.2-spark3.2-s_2.12"
-	SPARK_DELTA_JAR="io.delta:delta-core_2.12:1.0.0"
+    SPARK_VER="3.2.1"
+    JAVA_VER="11"
+    HADOOP_VER="3.2"
+    SCALA_VERSION="2.12.15"
+    SPARK_GRAPH_PKG="0.8.2-spark3.2-s_2.12/graphframes-0.8.2-spark3.2-s_2.12.jar"
+    SPARK_DELTA_PKG="delta-core_2.12/1.0.0/delta-core_2.12:1.0.0r"
+    SPARK_GRAPH_JAR="graphframes:graphframes:0.8.2-spark3.2-s_2.12"
+    SPARK_DELTA_JAR="io.delta:delta-core_2.12:1.0.0"
 elif [ $1 == '2' ] ; then
-	SPARK_VER="2.4.8"
-	JAVA_VER="8"
-	HADOOP_VER="2.7"
-	SCALA_VERSION="2.11.12"
-        SPARK_GRAPH_PKG="0.8.1-spark2.4-s_2.11/graphframes-0.8.1-spark2.4-s_2.11.jar" 
-        SPARK_DELTA_PKG="delta-core_2.11/0.6.1/delta-core_2.11-0.6.1.jar"
+    SPARK_VER="2.4.8"
+    JAVA_VER="8"
+    HADOOP_VER="2.7"
+    SCALA_VERSION="2.11.12"
+    SPARK_GRAPH_PKG="0.8.1-spark2.4-s_2.11/graphframes-0.8.1-spark2.4-s_2.11.jar" 
+    SPARK_DELTA_PKG="delta-core_2.11/0.6.1/delta-core_2.11-0.6.1.jar"
 else
-	echo "Script to build LASID Spark Docker image."
-	echo "Usage: ./build-images.sh <Spark> " 
-	echo "    <Spark> Spark major number version, i.e., 2 or 3" 
-	exit 1
+    echo "Script to build LASID Spark Docker image."
+    echo "Usage: ./build-images.sh <Spark> " 
+    echo "    <Spark> Spark major number version, i.e., 2 or 3" 
+    exit 1
 fi
 
 UBUNTU_VER="focal"
@@ -33,12 +33,12 @@ docker build --build-arg SP_VERSION=${SPARK_VER} --build-arg JV_VERSION=${JAVA_V
              -t lasid/spark-base:${SPARK_VER}_${UBUNTU_VER}  -t lasid/spark-base:latest ./docker/spark-base
 docker build --build-arg SP_VERSION=${SPARK_VER} -t lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER} \
              -t lasid/spark-worker:latest ./docker/spark-worker
-docker build -t lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER} -t lasid/spark-worker-tensorflow:latest \
-             ./docker/spark-worker-tensorflow
+#docker build -t lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER} -t lasid/spark-worker-tensorflow:latest \
+#             ./docker/spark-worker-tensorflow
 
 ## Push image to DockerHub
 docker login
 #docker push lasid/spark-worker:latest
 docker push lasid/spark-worker:${SPARK_VER}_${UBUNTU_VER}
 #docker push lasid/spark-worker-tensorflow:latest
-docker push lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}
+#docker push lasid/spark-worker-tensorflow:${SPARK_VER}_${UBUNTU_VER}
